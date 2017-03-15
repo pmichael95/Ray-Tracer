3f001fc2449c0a314a2aafdacee8e150eb66d8b1
PHILIP MICHAEL <phil-mik@hotmail.com>
2017-03-15 15:34:06 -0400

    Ray Tracer Project - Initial Push
    Initial push with git ignores and main driver.

---
+++ Ray-Tracer/Ray-Tracer/main.cpp
@@ -0,0 +1,17 @@
+/*
+* RAY TRACER MAIN DRIVER PROGRAM
+* [This project is still a Work in Progress!]
+* Programmed by Philip Michael
+*/
+#include <iostream>
+#include <string>
+#include <exception>
+
+using namespace std;
+
+int main() {
+
+	// Wait before ending
+	system("PAUSE");
+	return 0;
+}
